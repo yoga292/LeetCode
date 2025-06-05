276b96a2b6768602fd8a1de848b27ac8e5859fc3
yoga292 <yogapriyavision@gmail.com>
2025-06-05 22:57:02 +0530

    Create ValidParantheses.java

---
+++ ValidParantheses.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+class ValidParantheses{
+    public boolean isValid(String s) {
+       
+       Stack<Character> st = new Stack<>();
+       for(char ch:s.toCharArray())
+       {
+        if(ch=='(')
+         st.push(')');
+        else if (ch=='[')
+         st.push(']');
+        else if (ch=='{')
+         st.push('}');
+        else if(st.isEmpty()||ch!=st.pop())
+         return false;
+       }
+       return st.isEmpty();
+    }
+}
